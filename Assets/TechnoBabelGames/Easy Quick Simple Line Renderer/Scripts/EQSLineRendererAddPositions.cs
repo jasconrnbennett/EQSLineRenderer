06069ee18e8350dda7a2f8c674b42f1fc025a733
Jason <jasonbennett@gamerjibe.com>
2022-08-28 10:05:45 -0500

    Starting to script creating basic shapes

--- Assets/TechnoBabelGames/Easy Quick Simple Line Renderer/Scripts/EQSLineRendererAddPositions.cs
+++ Assets/TechnoBabelGames/Easy Quick Simple Line Renderer/Scripts/EQSLineRendererAddPositions.cs
@@ -7,10 +7,13 @@ namespace TechnoBabelGames
     public class EQSLineRendererAddPositions : MonoBehaviour
     {
         private Transform[] points;
+        public enum BasicShape { None, Line, Triangle}
+        public BasicShape basicShape;
 
         void Start()
         {
             SetPoints();
+            //DrawBasicShape(basicShape);
         }
 
         public void SetPoints()
@@ -25,10 +28,50 @@ namespace TechnoBabelGames
 
                 v3 = new Vector3(points[i].position.x, points[i].position.y, points[i].position.z);
                 lineRenderer.SetPosition(i, v3);
+            }            
+        }
 
+        private void DrawBasicShape(BasicShape shape)
+        {
+            if (shape == BasicShape.None)
+                return;
+
+            switch (shape)
+            {
+                case BasicShape.Line:
+
+                    Transform parentTransform = transform.GetComponentInParent<Transform>();
+
+                    if(parentTransform.rotation.x == 90f) //Alignment Y
+                    {
+
+                    }
+                    else if (parentTransform.rotation.y == 90f) //Alignment X
+                    {
+
+                    }
+                    else if (parentTransform.rotation.z == 90f) //Alignment Z
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+
+                    var transforms = transform.GetComponentsInChildren<Transform>().Skip(1).ToArray();
+                    for (int i = 1; i < transforms.Length; i++)
+                    {
+
+                    }
+
+                    break;
+                case BasicShape.Triangle:
+                    break;
+                default:
+                    break;
             }
 
-            //EditorWindow.GetWindow<SceneView>().Repaint();
         }
     }
 }
